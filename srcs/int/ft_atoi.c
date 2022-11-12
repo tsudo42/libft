bc17dc12d3def720eba63a9a655dcb19f0e3fd71
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-11-12 22:26:11 +0900

    Refactor: ft_atoi

--- srcs/int/ft_atoi.c
+++ srcs/int/ft_atoi.c
@@ -5,62 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/19 16:32:27 by tsudo             #+#    #+#             */
-/*   Updated: 2022/03/04 16:13:30 by tsudo            ###   ##########        */
+/*   Created: 2022/11/12 00:00:00 by tsudo             #+#    #+#             */
+/*   Updated: 2022/11/12 00:00:00 by tsudo            ###   ##########        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_int.h"
-#include "ft_string.h"
+#include "ft_char.h"
 
 /* ************************************************************************** */
-/*  This helper function compares two ulongs,                                 */
-/*  one is ulong representation of str, and the other is num.                 */
-/*  The abs of return value should not be used, use sign instead (like other  */
-/*  cmp functions).                                                           */
+/*  This helper function reads str and returns represented number.            */
+/*  If the number is more than ULONG_MAX, this function returns ULONG_MAX     */
 /* ************************************************************************** */
-static int	my_unsigned_nonzero_slcmp(const char *str, unsigned long num)
+static unsigned long	read_num(const char *str)
 {
-	size_t			str_len;
-	size_t			num_len;
-	unsigned long	tmp;
+	unsigned long	ul;
+	unsigned long	add;
 
-	num_len = 0;
-	tmp = num;
-	while (tmp > 0 && ++num_len > 0)
-		tmp /= 10;
-	str_len = 0;
-	while ('0' <= str[str_len] && str[str_len] <= '9' && str_len <= num_len)
-		str_len++;
-	if (num_len != str_len)
-		return (str_len - num_len);
-	tmp = 1;
-	while (num_len-- > 1)
-		tmp *= 10;
+	ul = 0;
 	while ('0' <= *str && *str <= '9')
 	{
-		if ((*str - '0') != (int)(num / tmp))
-			return ((*str - '0') - (num / tmp));
-		num %= tmp;
-		tmp /= 10;
-		str++;
+		add = *str - '0';
+		if ((ul > ULONG_MAX / 10) || \
+			(ul == ULONG_MAX / 10 && add > ULONG_MAX % 10))
+			return (ULONG_MAX);
+		ul = ul * 10 + add;
+		++str;
 	}
-	return (0);
-}
-
-/* ************************************************************************** */
-/*  This helper function returns LONG_MAX or LONG_MIN if num_str represents   */
-/*  over LONG_MAX with sign>0, or num_str represents over abs(LONG_MIN-1)     */
-/*  with sign<0, respectively.                                                */
-/*  Otherwise, this function returns 0.                                       */
-/* ************************************************************************** */
-static long	my_ato_llimits(const char *num_str, const int sign)
-{
-	if (sign > 0 && my_unsigned_nonzero_slcmp(num_str, LONG_MAX) >= 0)
-		return (LONG_MAX);
-	if (sign < 0 && my_unsigned_nonzero_slcmp(num_str, -(LONG_MIN + 1)) > 0)
-		return (LONG_MIN);
-	return (0);
+	return (ul);
 }
 
 /* ************************************************************************** */
@@ -71,24 +43,29 @@ static long	my_ato_llimits(const char *num_str, const int sign)
 /* ************************************************************************** */
 int	ft_atoi(const char *str)
 {
-	char	sign;
-	long	val;
+	int				sign;
+	unsigned long	num;
 
-	while (*str != '\0' && ft_strchr("\t\n\v\f\r ", *str) != NULL)
-		str++;
+	while (ft_isspace(*str))
+		++str;
 	sign = 1;
-	if ((*str == '+' || *str == '-') && *(str++) == '-')
-		sign = -1;
-	while (*str == '0')
-		str++;
-	val = my_ato_llimits(str, sign);
-	if (val != 0)
-		return ((int)val);
-	while ('0' <= *str && *str <= '9')
+	if (*str == '+' || *str == '-')
 	{
-		val *= 10;
-		val += *str - '0';
-		str++;
+		if (*str == '-')
+			sign = -1;
+		++str;
 	}
-	return ((int)(val * sign));
+	num = read_num(str);
+	if (sign > 0)
+	{
+		if (num > LONG_MAX)
+			return ((int)LONG_MAX);
+		return (num);
+	}
+	else
+	{
+		if (num > LONG_MAX)
+			return ((int)LONG_MIN);
+		return (-(long)num);
+	}
 }
